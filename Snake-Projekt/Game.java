e4f42c63d876e97cf5d3b9f7739705959f6d7c54
maxitaxi27 <96301909+maxitaxi27@users.noreply.github.com>
2021-12-17 15:50:10 +0100

    First two files committed

---
+++ Snake-Projekt/Game.java
@@ -0,0 +1,34 @@
+/*public class jeff{
+
+
+public static void Hallo(){
+    System.out.println("Du huan");
+}
+public static void main(String[] args)
+{
+    Hallo();
+}
+}
+*/
+
+
+
+public class Game {
+    public Game(){
+        System.out.println("Game has been created");
+    }
+  
+  
+
+    public static void main (String []args) {
+        System.out.println("Hello");
+        Game Spiel = new Game() ;
+        
+        
+}
+}
+
+
+  
+
+
---
+++ Snake-Projekt/src/Spiel.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Spiel{
+
+
+    public void StartGame(){
+        System.out.println("Starte das SPiel");
+    }
+}
